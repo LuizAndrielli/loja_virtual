225d1c75d73dd9fb7ebe0f583105ba370fbe70a5
Luiz Augusto <gutoandrielli@gmail.com>
2024-04-04 00:27:36 -0300

    Implementacao classes de model mentoria

--- src/main/java/jdev/mentoria/lojavirtual/model/Acesso.java
+++ src/main/java/jdev/mentoria/lojavirtual/model/Acesso.java
@@ -1,7 +1,8 @@
 package jdev.mentoria.lojavirtual.model;
 
-import java.io.Serializable;
 import java.util.Objects;
+
+import org.springframework.security.core.GrantedAuthority;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
@@ -12,18 +13,23 @@ import jakarta.persistence.SequenceGenerator;
 import jakarta.persistence.Table;
 
 @Entity
-@Table(name = "categoria_produto")
-@SequenceGenerator(name = "seq_categoria_produto", sequenceName = "seq_categoria_produto", initialValue = 1,allocationSize = 1)
-public class CategoriaProduto implements Serializable {
+@Table(name = "acesso")
+@SequenceGenerator(name = "seq_acesso", sequenceName = "seq_acesso", initialValue = 1, allocationSize = 1)
+public class Acesso implements GrantedAuthority {
 
 	private static final long serialVersionUID = 1L;
-	
+
 	@Id
-	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq_categoria_produto")
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq_acesso")
 	private Long id;
-	
-	@Column(name = "nome_desc", nullable = false)
-	private String nomeDesc;
+
+	@Column(nullable = false)
+	private String descricao; /* Acesso ex: ROLE_ADMIN ou ROLE_SECRETARIO */
+
+	@Override
+	public String getAuthority() {
+		return this.descricao;
+	}
 
 	public Long getId() {
 		return id;
@@ -33,12 +39,12 @@ public class CategoriaProduto implements Serializable {
 		this.id = id;
 	}
 
-	public String getNomeDesc() {
-		return nomeDesc;
+	public String getDescricao() {
+		return descricao;
 	}
 
-	public void setNomeDesc(String nomeDesc) {
-		this.nomeDesc = nomeDesc;
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
 	}
 
 	@Override
@@ -54,11 +60,8 @@ public class CategoriaProduto implements Serializable {
 			return false;
 		if (getClass() != obj.getClass())
 			return false;
-		CategoriaProduto other = (CategoriaProduto) obj;
+		Acesso other = (Acesso) obj;
 		return Objects.equals(id, other.id);
 	}
-	
-	
-	
 
 }
